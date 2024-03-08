7d79cc3922b788e4492129d23bc6721b6615b362
Benas Šimkūnas <benas.simkunas@ktu.edu>
2024-03-08 15:12:03 +0200

    Web: views/AppView: show profile information

--- Web/src/views/AppView.vue
+++ Web/src/views/AppView.vue
@@ -1,3 +1,23 @@
+<script setup>
+    import { ref } from 'vue';
+    import router from '../router';
+    import { getCookie } from '../utils/cookies.js'
+    import { logout, getProfileInfo } from '../utils/auth.js'
+
+    function doLogout(e) {
+        logout();
+        router.push({path: '/'});
+    }
+
+    const profile = getProfileInfo();
+
+    const name = ref(profile.name);
+    const email = ref(profile.email);
+</script>
+
 <template>
-  <h1>APP</h1>
+    <h1>APP</h1>
+    <b>vardas: {{name}}</b> <br/>
+    <b>el. paštas: {{email}}</b> <br/>
+    <button @click="doLogout">{{ ct }} Atsijungti</button>
 </template>
