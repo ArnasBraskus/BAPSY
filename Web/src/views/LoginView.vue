08b8d813c36fd9ba63d8bf2105013b0e4d5af574
Benas Šimkūnas <benas.simkunas@ktu.edu>
2024-03-08 15:11:43 +0200

    Web: views/LoginView: add google sign-in support

--- Web/src/views/LoginView.vue
+++ Web/src/views/LoginView.vue
@@ -1,4 +1,15 @@
+<script setup>
+    import router from '../router'
+    import { decodeCredential } from 'vue3-google-login'
+    import { getCookie, setCookie } from '../utils/cookies.js'
+
+    function loginCallback(res) {
+        setCookie('g-signin-credential', res.credential, 7 * 24 * 60 * 60);
+        router.push({path: 'app'});
+    }
+</script>
+
 <template>
   <h1>LOGIN</h1>
-  <RouterLink to="/app">App</RouterLink>
+  <GoogleLogin :callback="loginCallback"/>
 </template>
