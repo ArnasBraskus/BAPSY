2a66da01ca400d315635e483dc2a6639ddfbb2c3
Benas Šimkūnas <benas.simkunas@ktu.edu>
2024-03-08 15:11:03 +0200

    Web: main: use vue3_google_login plugin

--- Web/src/main.js
+++ Web/src/main.js
@@ -3,9 +3,14 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import vue3_google_login from 'vue3-google-login'
+import config from './config.js'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(vue3_google_login, {
+    clientId: config.googleSignInClientId
+});
 
 app.mount('#app')
