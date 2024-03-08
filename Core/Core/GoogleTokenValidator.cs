0d0488cbcfb534137342db7e3cd22166796b1c6f
Benas Šimkūnas <benas.simkunas@ktu.edu>
2024-03-08 19:53:18 +0200

    Core: authenticate using Google's JWT

---
+++ Core/Core/GoogleTokenValidator.cs
@@ -0,0 +1,33 @@
+using Google.Apis.Auth;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.IdentityModel.JsonWebTokens;
+
+public class GoogleTokenValidator : JsonWebTokenHandler {
+    private GoogleJsonWebSignature.ValidationSettings Settings;
+
+    public GoogleTokenValidator(string audience) {
+        Settings = new GoogleJsonWebSignature.ValidationSettings() {Audience = new string[]{audience}};
+    }
+    public override async Task<TokenValidationResult> ValidateTokenAsync(String token, TokenValidationParameters parameters) {
+        try {
+            var payload = await GoogleJsonWebSignature.ValidateAsync(token, Settings);
+
+            SecurityToken securityToken = ReadToken(token);
+
+            var claims = new List<Claim> {
+                new Claim(ClaimTypes.Role, "User"),
+                new Claim(ClaimTypes.NameIdentifier, payload.Name),
+                new Claim(ClaimTypes.Name, payload.Name),
+                new Claim(ClaimTypes.Email, payload.Email),
+                new Claim(ClaimTypes.GivenName, payload.GivenName),
+                new Claim(ClaimTypes.Surname, payload.FamilyName)
+            };
+
+            return new TokenValidationResult() { Issuer = payload.Issuer, ClaimsIdentity = new ClaimsIdentity(claims), SecurityToken = securityToken, IsValid = true };
+        }
+        catch (InvalidJwtException) {
+            return new TokenValidationResult { IsValid = false };
+        }
+    }
+}
--- Core/Core/Program.cs
+++ Core/Core/Program.cs
@@ -1,9 +1,31 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Security.Claims;
 
 Config conf = Config.Read("config.json");
 
 var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+
+services.AddAuthentication(options => {
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options => {
+    options.TokenHandlers.Clear();
+    options.TokenHandlers.Add(new GoogleTokenValidator(conf.GoogleApiClientId));
+});
+
+
+builder.Services.AddAuthorization(options => {
+    options.AddPolicy("Users", policy => policy.RequireClaim(ClaimTypes.Role, "User"));
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapGet("/", () => "Authenticated.").RequireAuthorization("Users");
+app.MapGet("/noauth", () => "No authentication.");
 
 app.Run();
