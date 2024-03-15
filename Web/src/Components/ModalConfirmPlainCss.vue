d32cd3ba2f29fc721faa94005f0091c680778b01
ArnasBraskus <arnbra1@ktu.lt>
2024-03-15 13:36:46 +0200

    BookPlan page with modal (pop-up)

---
+++ Web/src/Components/ModalConfirmPlainCss.vue
@@ -0,0 +1,98 @@
+<script setup lang="ts">
+import { VueFinalModal } from 'vue-final-modal'
+
+defineProps<{
+  title?: string
+}>()
+
+const emit = defineEmits<{
+  (e: 'confirm'): void
+}>()
+</script>
+
+<template>
+  <VueFinalModal
+    class="confirm-modal"
+    content-class="confirm-modal-content"
+    overlay-transition="vfm-fade"
+    content-transition="vfm-fade"
+  >
+    <h1>{{ title }}</h1>
+    <slot />
+    <p> Book name   
+    </p> 
+    <input />
+    <p> How many pages?
+    </p>
+    <input />  
+    <p> Book format </p> 
+    <select>
+         <option value="a4">a4</option>
+         <option value="a5">a5</option>
+         <option value="a5">a6</option>
+    </select>
+    <p> Deadline date</p>
+    <input type="date"/>
+    <p> Select which days to read </p>
+    <fieldset>
+    <div class="days">
+    <label>
+      <input type="checkbox" value="1" name="days"> Monday
+    </label>
+    <label>
+      <input type="checkbox" value="2" name="days"> Tuesday
+    </label>
+    <label>
+      <input type="checkbox" value="3" name="days"> Wednesday
+    </label>
+    <label>
+      <input type="checkbox" value="4" name="days"> Thursday
+    </label>
+    <label>
+      <input type="checkbox" value="5" name="days"> Friday
+    </label>
+    <label>
+      <input type="checkbox" value="6" name="days"> Saturday
+    </label>
+    <label>
+      <input type="checkbox" value="0" name="days"> Sunday
+    </label>
+    </div>
+    </fieldset>
+    <p>Input what hour you prefer to read</p>
+    <input type="time">
+    <button @click="emit('confirm')">
+      Confirm
+    </button>
+  </VueFinalModal>
+</template>
+
+<style>
+.confirm-modal {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.confirm-modal-content {
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+  background: #fff;
+  border-radius: 0.5rem;
+}
+.confirm-modal-content > * + *{
+  margin: 0.5rem 0;
+}
+.confirm-modal-content h1 {
+  font-size: 1.375rem;
+}
+.confirm-modal-content button {
+  margin: 0.25rem 0 0 auto;
+  padding: 0 8px;
+  border: 1px solid;
+  border-radius: 0.5rem;
+}
+.dark .confirm-modal-content {
+  background: #000000;
+}
+</style>
---
+++ Web/src/views/BookPlan.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+import { ModalsContainer, useModal } from 'vue-final-modal'
+import ModalConfirmPlainCss from '../Components/ModalConfirmPlainCss.vue'
+import { RouterView } from 'vue-router'
+import Header from '../Components/Header.vue'
+import Footer from '../Components/Footer.vue'
+
+const { open, close } = useModal({
+    component: ModalConfirmPlainCss,
+    attrs: {
+        title: 'Plan',
+        onConfirm() {
+            close()
+        },
+    },
+})
+</script>
+
+<template>
+     <div>
+        <header class="header">
+            <div class="logo"></div>
+            <nav>
+                <RouterLink to="/">Home</RouterLink>
+                <RouterLink to="/about">About</RouterLink>
+                <RouterLink to="/plan">Plan</RouterLink>
+                <RouterLink to="/books">Books</RouterLink>
+                <RouterLink to="/settings">Settings</RouterLink>
+                <!-- dar neveikia normaliai
+    <button v-if="isLoggedIn" @click="doLogout">Log Out</button>
+    <button v-else @click="doLogin">Log In</button>
+    -->
+            </nav>
+        </header>
+            <div id="app" class="image-container">
+                <main class="darker ">
+                    <div>
+                        <div>
+                            <div>
+                                <h1>Plan management</h1>
+                            </div>
+                            <div>
+                                <VButton class="cta-button" @click="open">
+                                    Create a Plan
+                                </VButton>
+                                <ModalsContainer />
+                            </div>
+                        </div>
+                        <RouterView />
+                    </div>
+                </main>
+                <Footer />
+            </div>
+</div>
+</template>
+
+<style scoped>
+
+    .home {
+        text-align: center;
+        color: var(--quaternary-color);
+        padding: 20px;
+        margin-top: 20px;
+        background-color: rgba(185, 148, 112, 0.75);
+        font-size: x-large;
+        border-radius: 50px 50px 50px 50px;
+    }
+</style>
