b5b4735940be0579da3b7ff4fc0a2464edc6dea4
ArnasBraskus <arnbra1@ktu.lt>
2024-03-15 13:33:36 +0200

    Added vue-final-modal

--- Web/src/main.js
+++ Web/src/main.js
@@ -1,13 +1,17 @@
 import './assets/main.css';
+import 'vue-final-modal/style.css'
 
 import { createApp } from 'vue';
+import { createVfm } from 'vue-final-modal'
 import App from './App.vue';
 import router from './router';
 import vue3_google_login from 'vue3-google-login';
 import config from './config.js';
 
 const app = createApp(App);
+const vfm = createVfm()
 
+app.use(vfm)
 app.use(router);
 app.use(vue3_google_login, {
   clientId: config.googleSignInClientId
