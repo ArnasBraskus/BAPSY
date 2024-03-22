c37c696f1a7fe3d2f06b00599b73d2a7f991634d
Benas Šimkūnas <benas.simkunas@ktu.edu>
2024-03-22 14:50:57 +0200

    Core: add ApiBase

---
+++ Core/Core/ApiBase.cs
@@ -0,0 +1,43 @@
+using System.Text.Json;
+
+public abstract class ApiBase {
+    private Users Users;
+
+    protected static IResult BadAuth = Results.BadRequest(new { Error = "Authentication error." });
+    protected static IResult BadJson = Results.BadRequest(new { Error = "Malformed JSON." });
+
+    public ApiBase(Users users) {
+        Users = users;
+    }
+
+    protected User? CheckAuth(HttpContext context) {
+        var email = Auth.GetNameIdentifier(context);
+
+        if (email == null)
+            return null;
+
+        User? user = Users.FindUser(email);
+
+        if (user == null)
+            return null;
+
+        return user;
+    }
+
+    private static readonly JsonSerializerOptions JsonOptions = new JsonSerializerOptions {
+        PropertyNameCaseInsensitive = true
+    };
+
+    protected async Task<T?> ReadJson<T>(HttpRequest request) where T : class {
+        var stream = await new StreamReader(request.Body).ReadToEndAsync();
+
+        try {
+            return JsonSerializer.Deserialize<T>(stream, JsonOptions);
+        }
+        catch (JsonException) {
+            return null;
+        }
+    }
+
+    public abstract void Map(WebApplication app);
+}
