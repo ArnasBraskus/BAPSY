2827d7fd6fd1f444af4827b50dacff89b18e2a04
Benas Šimkūnas <benas.simkunas@ktu.edu>
2024-03-22 14:59:00 +0200

    Core: add book plan API

--- Core/Core/Program.cs
+++ Core/Core/Program.cs
@@ -12,6 +12,7 @@ if (!dbExists) {
 }
 
 Users users = new Users(db);
+Plans plans = new Plans(db);
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -29,10 +30,14 @@ var app = builder.Build();
 app.UseAuthentication();
 app.UseAuthorization();
 
-AuthApi authApi = new AuthApi(auth, users);
-UserApi userApi = new UserApi(users);
+ApiBase[] apis = new ApiBase[] {
+    new AuthApi(auth, users),
+    new UserApi(users),
+    new BookPlanApi(users, plans)
+};
 
-userApi.Map(app);
-authApi.Map(app);
+foreach (var api in apis) {
+    api.Map(app);
+}
 
 app.Run();
