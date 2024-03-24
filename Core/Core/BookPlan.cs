38d47ca16484e3106ca15df51a660e461ee04b5c
Benas Šimkūnas <benas.simkunas@ktu.edu>
2024-03-24 17:31:52 +0200

    Core: move BookPlan to global namespace

--- Core/Core/BookPlan.cs
+++ Core/Core/BookPlan.cs
@@ -1,31 +1,28 @@
-﻿namespace Core
+﻿public class BookPlan
 {
-    public class BookPlan
-    {
-        public int Id { get;}
-        public int UserId { get;}
-        public string DeadLine { get;}
-        public int DayOfWeek { get;}    //ne viena ?
-        public string timeOfDay {  get;}    
-        public int PagesPerDay { get;}
-        public string Title { get; }
-        public string Author { get; }
-        public int PageCount { get; }
-        public int Size { get; }
+    public int Id { get;}
+    public int UserId { get;}
+    public string DeadLine { get;}
+    public int DayOfWeek { get;}    //ne viena ?
+    public string timeOfDay {  get;}    
+    public int PagesPerDay { get;}
+    public string Title { get; }
+    public string Author { get; }
+    public int PageCount { get; }
+    public int Size { get; }
 
-        public BookPlan(int id, int userId, string deadLine, int dayOfWeek, string timeOfDay, int pagesPerDay, 
-            string title, string author, int pageCount, int size)
-        {
-            Id = id;
-            UserId = userId;
-            DeadLine = deadLine;
-            DayOfWeek = dayOfWeek;
-            this.timeOfDay = timeOfDay;
-            PagesPerDay = pagesPerDay;
-            Title = title;
-            Author = author;
-            PageCount = pageCount;
-            Size = size;
-        }
+    public BookPlan(int id, int userId, string deadLine, int dayOfWeek, string timeOfDay, int pagesPerDay, 
+        string title, string author, int pageCount, int size)
+    {
+        Id = id;
+        UserId = userId;
+        DeadLine = deadLine;
+        DayOfWeek = dayOfWeek;
+        this.timeOfDay = timeOfDay;
+        PagesPerDay = pagesPerDay;
+        Title = title;
+        Author = author;
+        PageCount = pageCount;
+        Size = size;
     }
 }
