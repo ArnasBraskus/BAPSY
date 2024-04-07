6b3781b5caedc0ba3b48cc958e504d31cf18e6da
Benas Šimkūnas <benas.simkunas@ktu.edu>
2024-04-07 17:35:30 +0300

    Core: add ReadingSession

---
+++ Core/Core/ReadingSession.cs
@@ -0,0 +1,37 @@
+public class ReadingSession
+{
+    private ReadingSessions? ReadingSessions;
+    public int Id { get; }
+    public string Date { get; }
+    public int Goal { get; }
+
+    private int _Actual;
+    public int Actual
+    {
+        get {
+            return _Actual;
+        }
+        set {
+            if (ReadingSessions is null)
+                throw new InvalidOperationException("ReadingSession is not connected to database");
+
+            ReadingSessions.SetActual(Id, value);
+
+            _Actual = value;
+        }
+    }
+
+    public ReadingSession(string date, int goal) {
+        Date = date;
+        Goal = goal;
+    }
+
+    public ReadingSession(ReadingSessions events, int id, string date, int goal, int actual)
+    {
+        ReadingSessions = events;
+        Id = id;
+        Date = date;
+        Goal = goal;
+        _Actual = actual;
+    }
+}
