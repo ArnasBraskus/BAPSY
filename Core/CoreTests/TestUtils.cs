9b131a346cc53f6d5e455b3d8cfa15ba3c3ea0a1
Benas Šimkūnas <benas.simkunas@ktu.edu>
2024-04-08 16:51:53 +0300

    CoreTests: TestUtils: specify connection string

--- Core/CoreTests/TestUtils.cs
+++ Core/CoreTests/TestUtils.cs
@@ -1,7 +1,7 @@
 public class TestUtils
 {
     public static Database CreateDatabase() {
-        Database database = new Database();
+        Database database = new Database("Data Source=:memory:");
 
         database.Open();
         database.Create(DatabaseSchema.Schema);
