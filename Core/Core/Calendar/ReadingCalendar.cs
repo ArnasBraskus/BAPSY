b0e9b10c20dae5e565ecbb98d2e8cc6badacaf77
pijusgrinc <119979891+pijusgrinc@users.noreply.github.com>
2024-04-15 00:04:31 +0300

    Core: ReadingCalendar: added methods and exceptions

--- Core/Core/Calendar/ReadingCalendar.cs
+++ Core/Core/Calendar/ReadingCalendar.cs
@@ -13,6 +13,17 @@ public class ReadingCalendar
 
     public static ReadingCalendar Create(BookPlan plan)
     {
+        if (plan == null)
+            throw new ArgumentNullException(nameof(plan));
+
+        
+        if (plan.ReadingSessions == null)
+            throw new ArgumentNullException(nameof(plan.ReadingSessions));
+
+        if (plan.PagesRead > plan.PageCount)
+        throw new ArgumentException("Pages read cannot exceed total page count", nameof(plan.PagesRead));
+
+
         var events = new List<ReadingEvent>();
 
         ReadingCalendar calendar = new ReadingCalendar(plan.Title, plan.Author, events);
@@ -22,6 +33,7 @@ public class ReadingCalendar
 
         int pages = plan.PagesRead + 1;
 
+
         foreach (var session in plan.ReadingSessions)
         {
             var date = DateTime.Parse($"{session.Date} {plan.timeOfDay}");
@@ -29,11 +41,58 @@ public class ReadingCalendar
             if (date < DateTime.Today)
                 continue;
 
+            if (session.Goal < 0)
+                throw new ArgumentException("Page goal cannot be negative", nameof(session.Goal));
+
             events.Add(new ReadingEvent(date, pages, session.Goal));
 
             pages += session.Goal;
         }
 
         return calendar;
+    }
+
+    public void Update(BookPlan plan)
+    {
+        if (plan == null)
+        throw new ArgumentNullException(nameof(plan));
+
+        Events.Clear();
+
+        if (plan.PagesRead == plan.PageCount)
+            return;
+
+        int pages = plan.PagesRead + 1;
+
+        if (plan.ReadingSessions == null)
+            throw new ArgumentNullException(nameof(plan.ReadingSessions));
+
+        foreach (var session in plan.ReadingSessions)
+        {
+            var date = DateTime.Parse($"{session.Date} {plan.timeOfDay}");
+
+            if (date < DateTime.Today)
+                continue;
+
+            if (session.Goal < 0)
+                throw new ArgumentException("Page goal cannot be negative", nameof(session.Goal));
+
+
+            AddEvent(new ReadingEvent(date, pages, session.Goal));
+
+            pages += session.Goal;
+        }
+    }
+
+    public void RemoveEvent(ReadingEvent readingEvent)
+    {
+        if (Events.Contains(readingEvent))
+            Events.Remove(readingEvent);
+    }
+
+    public void AddEvent(ReadingEvent readingEvent)
+    {
+        if(readingEvent != null)
+            Events.Add(readingEvent);
     }
 }
