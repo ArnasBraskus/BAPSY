c3228bc50cee9638e2d99d700cb739f1fbb3a272
pijusgrinc <119979891+pijusgrinc@users.noreply.github.com>
2024-05-21 16:09:36 +0300

    Web: CalendarView.vue: Add button to open Google Calendar import site and button to download calendar file

--- Web/src/views/CalendarView.vue
+++ Web/src/views/CalendarView.vue
@@ -1,28 +1,40 @@
 <script setup>
-import { ref, } from 'vue';
+import { ref } from 'vue';
 import Menu from '../Components/Menu.vue';
 
 import { getToken } from '../utils/calendar.js';
 
 async function getCalendarUrl() {
   const token = await getToken();
-
   return `${window.location.origin}/api/calendar/${token.userId}/export?t=${token.token}`;
 }
 
 const url = ref('');
 
-(async() => {
+(async () => {
   url.value = await getCalendarUrl();
 })();
 
+const googleCalendarUrl = ref('https://calendar.google.com/calendar/r/settings/export');
+
+function goToGoogleCalendar() {
+  window.open(googleCalendarUrl.value, '_blank', 'noopener,noreferrer');
+}
+
+function goToUrl() {
+  window.open(url.value, '_blank', 'noopener,noreferrer');
+}
 </script>
 
 <template>
   <div id="app" class="plan-background">
-    <Menu/>
+    <Menu/>    
     <main>
-      <a :href="url">{{ url }}</a>
+      <br>
+      <button class="cta-button" @click="goToUrl">Download calendar file</button>
+      <br>
+      <button class="cta-button" @click="goToGoogleCalendar">Go to Google Calendar</button>
+      <br>      
     </main>
   </div>
 </template>
